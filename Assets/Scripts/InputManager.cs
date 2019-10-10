d08a3281f9f62d0c21863c7b7a05047197db2c06
adrideandrade <adriandeandrade98@gmail.com>
2019-10-09 21:29:56 -0400

    First Commit

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    // Inspector Variable
+    [Header ("Input Manager Settings")]
+    public string verticalAxis = "Vertical";
+    public string horizontalAxis = "Horizontal";
+    public KeyCode jumpButton = KeyCode.Space;
+
+    // Private Variables
+    [HideInInspector] public float horizontalInput;
+    [HideInInspector] public float verticalInput;
+    [HideInInspector] public Vector2 movement;
+
+    // Components
+
+    private void Update ()
+    {
+        horizontalInput = Input.GetAxisRaw(horizontalAxis);
+        verticalInput = Input.GetAxisRaw(verticalAxis);
+
+        movement = new Vector2(horizontalInput, verticalInput);
+        movement.y = 0;
+    }
+}
---
+++ Assets/Scripts/Toolbox.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toolbox : MonoBehaviour
+{
+    private InputManager inputManager;
+
+    #region Singleton
+    public static Toolbox instance;
+
+    private void InitSingleton ()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad (gameObject);
+    }
+    #endregion
+
+    private void Awake ()
+    {
+        InitSingleton();
+
+        inputManager = gameObject.AddComponent<InputManager>();
+    }
+
+    public InputManager GetInputManager ()
+    {
+        return inputManager;
+    }
+}
